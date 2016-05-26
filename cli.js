c5246ff42068ac2e28e8ea2a8dc85ceda9f64bd3
Manraj Singh <manrajsinghgrover@gmail.com>
2016-05-26 22:07:06 +0530

    Request working

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 * @Author: Manraj Singh
 * @Date:   2016-05-26 21:51:20
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-05-26 21:51:48
+* @Last Modified time: 2016-05-26 22:03:43
 */
 
 'use strict';
@@ -13,14 +13,18 @@ const chalk = require('chalk');
 const request = require('request');
 const config = require('./config');
 
-const COMPILE_URL = 'http://api.hackerearth.com/code/compile/';
-const RUN_URL = 'http://api.hackerearth.com/code/run/';
+const RUN_URL = 'http://api.hackerrank.com/checker/submission.json';
+
+var source = 'print 1';
 
 var data = {
-  'client_secret': config.CLIENT_SECRET,
+  'api_key': config.api_key,
   'source': source,
-  'lang': 'PYTHON',
+  'lang': 5,
+  'testcases': '["1"]'
 };
+
+console.log(data);
 
 request.post({ url : RUN_URL, form : data}, function(err,response){
   if(err){
