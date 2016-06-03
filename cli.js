a8c1dd05d325e6bcf36a0270e42196873c45703b
Manraj Singh <manrajsinghgrover@gmail.com>
2016-06-03 20:17:30 +0530

    Minor modifications

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 * @Author: Manraj Singh
 * @Date:   2016-05-26 21:51:20
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-05-26 22:03:43
+* @Last Modified time: 2016-06-03 20:16:53
 */
 
 'use strict';
@@ -15,9 +15,11 @@ const config = require('./config');
 
 const RUN_URL = 'http://api.hackerrank.com/checker/submission.json';
 
-var source = 'print 1';
 
-var data = {
+
+// var source = 'print 1';
+
+/* var data = {
   'api_key': config.api_key,
   'source': source,
   'lang': 5,
@@ -33,4 +35,4 @@ request.post({ url : RUN_URL, form : data}, function(err,response){
   else{
     console.log(response);
   }
-});
+});*/
