7c6847fae251c38fdc31cc2f330f335affe5696b
Manraj Singh <manrajsinghgrover@gmail.com>
2016-06-04 15:11:26 +0530

    Languages JSON updated

--- cli.js
+++ cli.js
@@ -4,12 +4,13 @@
 * @Author: Manraj Singh
 * @Date:   2016-05-26 21:51:20
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-06-04 11:51:59
+* @Last Modified time: 2016-06-04 15:10:43
 */
 
 'use strict';
 
 const yargs = require('yargs');
+const fs = require('fs');
 const ora = require('ora');
 const chalk = require('chalk');
 const request = require('request');
@@ -17,31 +18,39 @@ const config = require('./config');
 
 const RUN_URL = 'http://api.hackerrank.com/checker/submission.json';
 
-var run = function(yargs){
-
-}
-
-var changeConfig = function(yargs){
-
-}
-
 const argv = yargs
   .usage('$0 <command>')
   .command('run', 'Run code on HackerRank server', function(yargs){
     argv = yargs
-      .usage(chalk.green('usage: $0 run <options>'))
+      .usage('Usage: $0 run <options>')
       .demand(['s', 'i', 'o'])
       .alias('s', 'source').describe('s', 'Source Code file path')
       .alias('i', 'input').describe('i', 'Input file path')
-      .alias('l', 'language').describe('l', 'Language')
+      .alias('l', 'language').describe('l', 'Language. Change `config` for default.')
       .alias('o', 'output').describe('o', 'Output file path')
       .example('$0 run -s A.cpp -i Input00.in -o Output.txt -l CPP')
+      .argv;
   })
   .command('config', 'Change config file', function(yargs){
-    argv = yargs
-      .usage(chalk.green('usage: $0 config [options]'))
-      .alias('l', 'language').describe('l', 'List language codes')
+    var argv = yargs
+      .usage('Usage: $0 config [options]')
+      .alias('l', 'list').describe('l', 'List language and their code').boolean('l')
       .example('$0 config -l')
+      .argv;
+
+    if (argv.list){
+      const spinner = ora('Getting languages').start();
+      request('http://api.hackerrank.com/checker/languages.json', (error, response, body)=> {
+        if (!error && response.statusCode === 200) {
+          var languages = JSON.parse(body)
+          spinner.stop();
+          console.log(languages);
+        } else {
+          spinner.stop();
+          console.log(error);
+        }
+      });
+    }
   })
   .help('h')
   .alias('h', 'help')
