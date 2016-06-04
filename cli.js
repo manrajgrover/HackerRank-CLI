780906db1ebe10e0ec0389d4f9a125388d2de877
Manraj Singh <manrajsinghgrover@gmail.com>
2016-06-04 11:36:40 +0530

    Yargs is the best!

--- cli.js
+++ cli.js
@@ -4,11 +4,12 @@
 * @Author: Manraj Singh
 * @Date:   2016-05-26 21:51:20
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-06-03 21:34:28
+* @Last Modified time: 2016-06-04 11:30:01
 */
 
 'use strict';
 
+const yargs = require('yargs');
 const ora = require('ora');
 const chalk = require('chalk');
 const request = require('request');
@@ -16,24 +17,29 @@ const config = require('./config');
 
 const RUN_URL = 'http://api.hackerrank.com/checker/submission.json';
 
-const cli = meow(`
-  Usage
-    $ hackerrank run
+var run = function(yargs){
 
-  Options
-    -i, --input  Input file path
-    -o, --output  Output file path
-    -s, --source  Source code file path
-    -l, --language  Language
+}
 
-`, {
-  alias: {
-    i: 'input',
-    o: 'output',
-    s: 'source',
-    l: 'language'
-  }
-});
+var changeConfig = function(yargs){
+
+}
+
+const argv = yargs
+  .usage('$0 <command>')
+  .command('run', 'Run code on HackerRank server', function(yargs){
+    argv = yargs
+      .usage(chalk.blue('usage: $0 run <options>'))
+      .demand(['s', 'i', 'l', 'o'])
+      .example('$0 run -s CodeJamA.cpp -i Input00.in -o Output.txt -l CPP')
+  })
+  .command('config', 'Change config file', function(yargs){
+
+  })
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
 
 // var source = 'print 1';
 
